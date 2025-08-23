c7129c3e37ac4899a638f0a21333e5ca942f127b
Brayan Guerrero <brayan.guerrero.dt@gmail.com>
2025-08-23 11:38:30 -0500

    fade in animation

--- src/components/inicio/inicio.tsx
+++ src/components/inicio/inicio.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './inicio.css';
+import '../../common.css';
 
 export const Inicio: React.FC = () => {
   return (
-    <section className="presentation">
-      <h2 className="presentation-title">
-        Arte macabro y psicológico
-      </h2>
-      <p className="presentation-description">
-        Explora la belleza de lo macabro y la realidad de la psique trastornada.
-      </p>
-    </section>
+    <>
+      <section className="presentation">
+        <h2 className="presentation-title fade-in">
+          Arte macabro y psicológico
+        </h2>
+        <p className="presentation-description fade-in">
+          Explora la belleza de lo macabro y la realidad de la psique trastornada.
+        </p>
+      </section>
+      <section className="artistic-formats">
+        <h2 className='section-title'>Diversas formas de expresión</h2>
+        <p className='section-description'>Cada idea requiere su propia forma de expresarse.</p>
+      </section>
+    </>
   );
 };
