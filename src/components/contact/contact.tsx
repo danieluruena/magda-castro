72e49d1f6b571c1327e6d23c0dc2c44caeb1925f
BrayanZarabanda <brayan.guerrero.dt@gmail.com>
2026-02-22 12:39:43 -0500

    Front de contacto

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -4,13 +4,29 @@ import './contact.css'
 export const Contact: React.FC = () => {
   return (
     <section id="contact" className="contact">
-      <h3 className="contact-title">Contact</h3>
-      <p className="contact-text">
-        Let's work together on your next project
-      </p>
-      <a href="mailto:hello@magdacastro.com" className="contact-button">
-        Get in Touch
-      </a>
+      <article className="contact-layout">
+        <section className="contact-info">
+          <h3 className="contact-title">CONTÁCTANOS</h3>
+          <p className="contact-text">
+            Susúrranos tu idea… y la transformaremos en la obra que tu sombra está buscando.
+          </p>
+        </section>
+
+        <section className="contact-form">
+          <h3 className="contact-form-title">Solicita Información</h3>
+          <form className="form">
+            <input type="text" placeholder="Nombre" />
+            <input type="tel" placeholder="Telefono" />
+            <input type="email" placeholder="E-mail" />
+            <textarea
+              placeholder="Mensaje corto"
+              maxLength={220}
+              rows={3}
+            />
+            <a href="mailto:hello@magdacastro.com" className="contact-button">Enviar</a>
+          </form>
+        </section>
+      </article>
     </section>
   )
 }
