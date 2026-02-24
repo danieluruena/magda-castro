54a60f69a9cc9ee29e181c15b98bc49fa6641b9a
BrayanZarabanda <brayan.guerrero.dt@gmail.com>
2026-02-23 20:49:00 -0500

    Feat/contact:Data de formulario-Back

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import './contact.css'
 
 export const Contact: React.FC = () => {
+  const { register, handleSubmit } = useForm()  //función para obtener info de inputs después de envio 
+  const send = (data: any) => {  //función enviar datos a backend, por ahora solo en consola -- Rev data:any
+    console.log(data)
+  }
+
   return (
     <section id="contact" className="contact">
       <article className="contact-layout">
         <section className="contact-info">
           <h3 className="contact-title">CONTÁCTANOS</h3>
+
           <p className="contact-text">
             Susúrranos tu idea… y la transformaremos en la obra que tu sombra está buscando.
           </p>
@@ -14,16 +21,18 @@ export const Contact: React.FC = () => {
 
         <section className="contact-form">
           <h3 className="contact-form-title">Solicita Información</h3>
-          <form className="form">
-            <input type="text" placeholder="Nombre" />
-            <input type="tel" placeholder="Telefono" />
-            <input type="email" placeholder="E-mail" />
+
+          <form className="form" onSubmit={handleSubmit(send)}>
+            <input type="text" placeholder="Nombre" {...register('name')} /> 
+            <input type="phone" placeholder="Telefono" {...register('phone')} />
+            <input type="email" placeholder="Email" {...register('email')} />
             <textarea
               placeholder="Mensaje corto"
               maxLength={220}
               rows={3}
-            />
-            <a href="mailto:hello@magdacastro.com" className="contact-button">Enviar</a>
+              {...register('message')}/>
+
+            <button className="contact-button" type="submit">Enviar</button>
           </form>
         </section>
       </article>
