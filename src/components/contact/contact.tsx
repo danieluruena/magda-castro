27e60b0a01bcb26f6dad3fd4c38bc112805715fb
BrayanZarabanda <brayan.guerrero.dt@gmail.com>
2026-02-27 08:10:30 -0500

    feat(contact): update contact v2

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -10,32 +10,29 @@ export const Contact: React.FC = () => {
 
   return (
     <section id="contact" className="contact">
-      <article className="contact-layout">
+      <div className="contact-layout">
         <section className="contact-info">
           <h3 className="contact-title">CONTÁCTANOS</h3>
 
           <p className="contact-text">
-            Susúrranos tu idea… y la transformaremos en la obra que tu sombra está buscando.
+            ¿Tienes una idea que quiere hacer realidad con un estilo macabro o visceral?
           </p>
         </section>
 
         <section className="contact-form">
-          <h3 className="contact-form-title">Solicita Información</h3>
-
           <form className="form" onSubmit={handleSubmit(send)}>
             <input type="text" placeholder="Nombre" {...register('name')} /> 
-            <input type="phone" placeholder="Telefono" {...register('phone')} />
+            <input type="phone" placeholder="Télefono" {...register('phone')} />
             <input type="email" placeholder="Email" {...register('email')} />
             <textarea
-              placeholder="Mensaje corto"
-              maxLength={220}
-              rows={3}
+              placeholder="Me gustaría una escultura / pintura / personaje sobre.."
+              rows={6}
               {...register('message')}/>
 
             <button className="contact-button" type="submit">Enviar</button>
           </form>
         </section>
-      </article>
+      </div>
     </section>
   )
 }
