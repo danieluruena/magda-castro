c1ac927e53aa910247ebf0b5941afea07305b9ee
Daniel Urueña <daniel.uruena@hotmail.com>
2026-03-23 12:41:52 -0500

    fix netlify form

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -3,31 +3,31 @@ import './contact.css'
 import '../../common.css'
 
 export const Contact: React.FC = () => {
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault()
 
-    const form = e.currentTarget
-    const data = new FormData(form)
-    const encoded = new URLSearchParams(data as any).toString()
+  //   const form = e.currentTarget
+  //   const data = new FormData(form)
+  //   const encoded = new URLSearchParams(data as any).toString()
 
-    try {
-      const response = await fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: encoded,
-      })
+  //   try {
+  //     const response = await fetch('/', {
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  //       body: encoded,
+  //     })
 
-      if (!response.ok) {
-        console.error(`Error al enviar el mensaje: ${response.statusText}`)
-        throw new Error('Error al enviar el mensaje')
-      }
+  //     if (!response.ok) {
+  //       console.error(`Error al enviar el mensaje: ${response.statusText}`)
+  //       throw new Error('Error al enviar el mensaje')
+  //     }
 
-      console.log('Mensaje enviado correctamente')
-      form.reset()
-    } catch (error) {
-      console.error(error)
-    }
-  }
+  //     console.log('Mensaje enviado correctamente')
+  //     form.reset()
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }
   
   return (
     <section id="contact" className="contact">
@@ -41,16 +41,14 @@ export const Contact: React.FC = () => {
         </div>
         <div className="contact-form">
           <form 
-            onSubmit={handleSubmit}
-            method='POST'
-            action='/'
+            // onSubmit={handleSubmit}
+            method='post'
             className="form"
             name="contact">
+            <input type="hidden" name="form-name" value="contact" />
             <input type="text" placeholder="Nombre" name='name'/>
             <input type="tel" placeholder="Teléfono" name='phone'/>
             <input type="email" placeholder="Email" name='email'/>
-            <input type="hidden" name="bot-field" />
-            <input type="hidden" name="form-name" value="contact" />
             <textarea
               placeholder="Me gustaría una escultura / pintura / personaje sobre…"
               rows={6}
